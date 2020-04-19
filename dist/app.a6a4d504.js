310c034fb8eb6035b65fcdb07bb17e68c073d16c
qiudingding <272312267@qq.com>
2020-04-19 16:51:58 +0800

    更新 button里面 icon 的样式

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12476,7 +12476,9 @@ exports.default = _default;
       class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
     },
     [
-      _vm.icon ? _c("g-icon", { attrs: { name: _vm.icon } }) : _vm._e(),
+      _vm.icon
+        ? _c("g-icon", { staticClass: "icon", attrs: { name: _vm.icon } })
+        : _vm._e(),
       _vm._v(" "),
       _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
     ],
@@ -12627,7 +12629,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52247" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60962" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
